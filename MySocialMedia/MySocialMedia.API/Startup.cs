2249216e08a2cb2fabad1153937d8c8f2d79af3a
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-07-31 09:19:44 +0300

    Change Program file to 'Up Model' with another File Startup

---
+++ MySocialMedia/MySocialMedia.API/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySocialMedia.Logic.Services;
+
+namespace MySocialMedia.API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => 
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
