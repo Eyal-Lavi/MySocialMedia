e8b3a93ad9b18b843ece3b8cf720ebdd92e71a19
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-07-31 14:54:28 +0300

    Adding classes that represent the database

---
+++ MySocialMedia/MySocialMedia.Common/DBTables/User.cs
@@ -0,0 +1,34 @@
+﻿using MySocialMedia.Common.Validations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySocialMedia.Common.DBTables
+{
+    [Table("users")] // הגדרת שם הטבלה במסד הנתונים
+    public class User //ישות להוציא ולהכניס משתמש למסד נתונתים
+    {
+        public int ID { get; set; } // מזהה ייחודי של המשתמש
+        [Required]
+        public string? FIRST_NAME { get; set; } // שם פרטי
+        [Required]
+        public string? LAST_NAME { get; set; } // שם משפחה
+        [Required]
+        [StringLength(16,MinimumLength = 2)]
+        public string? USER_NAME { get; set; } // שם משתמש
+        [Required]
+        [StringLength(16)]
+        [OneCharacterIsBigLetter]
+        [OneCharacterIsSmallLetter]
+        public string? PASSWORD { get; set; } // סיסמה
+        [Required]
+        public DateTime DATE_CREATE { get; set; } // תאריך יצירת החשבון
+
+        public virtual ICollection<User_messages>? send_user_messages { get; set; } // הודעות שנשלחו
+        public virtual ICollection<User_messages>? receive_user_messages { get; set; } // הודעות שהתקבלו
+    }
+}
---
+++ MySocialMedia/MySocialMedia.Common/DBTables/Users_messages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySocialMedia.Common.DBTables
+{
+    [Table("users_messages")] // הגדרת שם הטבלה במסד הנתונים
+    public class User_messages
+    {
+        public int ID { get; set; } // מזהה ייחודי של ההודעה
+        public int SENDER_USER_ID { get; set; } // מזהה המשתמש ששלח את ההודעה
+        public int RECEIVER_USER_ID { get; set; } // מזהה המשתמש שקיבל את ההודעה
+        public string? MESSAGE_DATA { get; set; } // תוכן ההודעה
+        public DateTime MESSAGE_DATE { get; set; } // תאריך שליחת ההודעה
+        public virtual User? send_user { get; set; } // משתמש ששלח את ההודעה
+        public virtual User? receive_user { get; set; } // משתמש שקיבל את ההודעה
+    }
+}
