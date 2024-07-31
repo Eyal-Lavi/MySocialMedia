4e24aa4d36a2542729d4ac6c1a771c41253cdc66
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-07-31 14:55:37 +0300

    Build 2 Validations Attribute

---
+++ MySocialMedia/MySocialMedia.Common/Validations/OneCharacterIsBigLetterAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySocialMedia.Common.Validations
+{
+    public class OneCharacterIsBigLetterAttribute : ValidationAttribute
+    {
+        public OneCharacterIsBigLetterAttribute()
+        {
+        }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var target = value as string;
+
+            if (string.IsNullOrEmpty(target))
+            {
+                return ValidationResult.Success;
+            }
+            var haveBig = target.Any(x => x >= 'A' && x <= 'Z');
+
+            if (!haveBig)
+            {
+                return new ValidationResult("Must to be one character that is big letter");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ MySocialMedia/MySocialMedia.Common/Validations/OneCharacterIsSmallLetterAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySocialMedia.Common.Validations
+{
+    public class OneCharacterIsSmallLetterAttribute : ValidationAttribute
+    {
+        public OneCharacterIsSmallLetterAttribute()
+        {
+        }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var target = value as string;
+
+            if (string.IsNullOrEmpty(target))
+            {
+                return ValidationResult.Success;
+            }
+            var haveSmall = target.Any(x => x >= 'a' && x <= 'z');
+
+            if (!haveSmall)
+            {
+                return new ValidationResult("Must to be one character that is small letter");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
