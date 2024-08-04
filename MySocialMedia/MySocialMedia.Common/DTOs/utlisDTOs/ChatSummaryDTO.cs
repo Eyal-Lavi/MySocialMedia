fb37e00566225847e563a9cdf8ff84ec5fc486b6
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-08-04 04:45:33 +0300

    Add LoginResponseDTO and ChatSummaryDTO

---
+++ MySocialMedia/MySocialMedia.Common/DTOs/utlisDTOs/ChatSummaryDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySocialMedia.Common.DTOs.utlisDTOs
+{
+    public class ChatSummaryDTO
+    {
+        public int ChatId { get; set; }
+        public string ChatName { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ MySocialMedia/MySocialMedia.Common/DTOs/utlisDTOs/LoginResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySocialMedia.Common.DTOs.utlisDTOs
+{
+    public class LoginResponseDTO
+    {
+        public int UserId { get; set; }
+        public string AuthToken { get; set; }
+        public List<ChatSummaryDTO> Chats { get; set; }
+    }
+}
