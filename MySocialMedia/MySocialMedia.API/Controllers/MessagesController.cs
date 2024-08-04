3b47311de76a88ac5fa454cb12b553265d493d11
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-08-04 05:00:34 +0300

    Add MessagesController and UserController

---
+++ MySocialMedia/MySocialMedia.API/Controllers/MessagesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MySocialMedia.Common.DBTables.MessageDTOs;
+using MySocialMedia.Logic.Services;
+
+namespace MySocialMedia.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/messages")]
+    public class MessagesController : ControllerBase
+    {
+        private readonly IMessageService _messageService;
+        private readonly ILogger _logger;
+
+        public MessagesController(IMessageService messageService , ILogger<MessagesController> logger)
+        {
+            _messageService = messageService;
+            _logger = logger;
+        }
+        [HttpPost("send")]
+        public async Task<ActionResult> SendMessage([FromBody] UserMessageCreationDTO userMessageCreationDTO) 
+        {
+            var usernameClaim = User.Claims.FirstOrDefault(x => x.Type == "username");
+            var username = usernameClaim?.Value;
+            Console.WriteLine(username);
+
+            await _messageService.SendMessage(userMessageCreationDTO);
+
+            return Ok(true);
+        }
+        [HttpGet("allMessages")]
+        public async Task<ActionResult> GetAllChatWith([FromQuery] string targetUser)
+        {
+            var myUserNameClaim = User.Claims.FirstOrDefault(x => x.Type == "username");
+            var currentUser = myUserNameClaim?.Value;
+
+            if (string.IsNullOrEmpty(currentUser))
+            {
+                _logger.LogWarning("The controller has been verified but cannot find its username according to the token");
+                return Unauthorized();
+            }
+
+            var messages = await _messageService.GetMessagesForSpesificUser(currentUser ,targetUser);
+
+            var logValue = messages == null || !messages.Any() ?
+                $"No messages found between {currentUser} and {targetUser}." :
+                $"Have {messages.Count} messages ";
+
+            _logger.LogWarning(logValue);
+            return Ok(messages);
+        }
+    }
+}
---
+++ MySocialMedia/MySocialMedia.API/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MySocialMedia.Common.DTOs.UserDTOs;
+using MySocialMedia.Logic;
+using MySocialMedia.Logic.Services;
+
+namespace MySocialMedia.API.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly MySocialMediaDBContext _context;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(IUserService userService,ILogger<UserController> logger)
+        {
+            _userService = userService;
+            _logger = logger;
+        }
+        [HttpPost]
+        [Route("register")]
+        public async Task<ActionResult> Register([FromBody] UserCreationDTO userCreationDTO)
+        {
+            var result = await _userService.UserRegister(userCreationDTO);
+            if (!result)
+            {
+                return BadRequest("Username already exists");//400
+            }
+            
+            return Created();//201
+        }
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<bool>> Login([FromBody] UserLoginDTO userLoginDTO)
+        {
+            var userResponse = await _userService.UserLogin(userLoginDTO);
+
+            if (userResponse == null)
+            {
+                return NotFound();
+            }
+            return Ok(userResponse);//200
+        }
+    }
+}
