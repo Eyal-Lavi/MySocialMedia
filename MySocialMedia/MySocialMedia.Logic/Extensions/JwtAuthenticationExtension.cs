8ba9879a7eca5d130de894716ede10041993ece7
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-08-04 04:53:11 +0300

    Add JWT Authentication and Token Service

---
+++ MySocialMedia/MySocialMedia.Logic/Extensions/JwtAuthenticationExtension.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using MySocialMedia.Logic.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySocialMedia.Logic.Extensions
+{
+    public static class JwtAuthenticationExtension
+    {
+        public static void AddJwtAuthenticaion(this IServiceCollection services , IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("Jwt");
+            var secret = jwtSettings["Secret"];
+            var issuer = jwtSettings["Issuer"];
+            var audience = jwtSettings["Audience"];
+
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret))
+                };
+            });
+
+            services.AddSingleton<ITokenService>(new TokenService(secret, issuer, audience));
+        }
+    }
+}
---
+++ MySocialMedia/MySocialMedia.Logic/Services/TokenService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySocialMedia.Logic.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly string _secret;
+        private readonly string _issuer;
+        private readonly string _audience;
+
+        public TokenService(string secret, string issuer, string audience)
+        {
+            _secret = secret;
+            _issuer = issuer;
+            _audience = audience;
+        }
+        public string GenerateToken(string userName)
+        {
+
+            //מידע ומזהה יחודי לטוקן
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userName),
+                new Claim("username", userName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            
+            //יצירת מפתח אבטחה סימטרי על בסיס (_secret). מפתח זה ישמש לחתימה על הטוקן
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secret));
+
+            //יצירת האישורים לחתימה (signing credentials) על ידי שימוש במפתח הסימטרי ובאלגוריתם HMAC SHA256.
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                    _issuer,
+                    _audience,
+                    claims,// מידע ומזהה יחודי לטוקן
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds //האישורים לחתימה
+                    );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
