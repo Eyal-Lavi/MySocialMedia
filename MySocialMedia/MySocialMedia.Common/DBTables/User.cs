0a25187474ee9a967e6d460b48a5afe9bd7ee4bb
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-08-04 04:06:06 +0300

    Update User and UserMessage classes

--- MySocialMedia/MySocialMedia.Common/DBTables/User.cs
+++ MySocialMedia/MySocialMedia.Common/DBTables/User.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace MySocialMedia.Common.DBTables
@@ -28,7 +29,9 @@ namespace MySocialMedia.Common.DBTables
         [Required]
         public DateTime DATE_CREATE { get; set; } // תאריך יצירת החשבון
 
-        public virtual ICollection<User_messages>? send_user_messages { get; set; } // הודעות שנשלחו
-        public virtual ICollection<User_messages>? receive_user_messages { get; set; } // הודעות שהתקבלו
+        [JsonIgnore]
+        public virtual ICollection<UserMessage>? send_user_messages { get; set; } // הודעות שנשלחו
+        [JsonIgnore]
+        public virtual ICollection<UserMessage>? receive_user_messages { get; set; } // הודעות שהתקבלו
     }
 }
--- MySocialMedia/MySocialMedia.Common/DBTables/UserMessage.cs
+++ MySocialMedia/MySocialMedia.Common/DBTables/UserMessage.cs
@@ -4,19 +4,22 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace MySocialMedia.Common.DBTables
 {
     [Table("users_messages")] // הגדרת שם הטבלה במסד הנתונים
-    public class User_messages
+    public class UserMessage
     {
         public int ID { get; set; } // מזהה ייחודי של ההודעה
         public int SENDER_USER_ID { get; set; } // מזהה המשתמש ששלח את ההודעה
         public int RECEIVER_USER_ID { get; set; } // מזהה המשתמש שקיבל את ההודעה
         public string? MESSAGE_DATA { get; set; } // תוכן ההודעה
         public DateTime MESSAGE_DATE { get; set; } // תאריך שליחת ההודעה
+        [JsonIgnore]
         public virtual User? send_user { get; set; } // משתמש ששלח את ההודעה
+        [JsonIgnore]
         public virtual User? receive_user { get; set; } // משתמש שקיבל את ההודעה
     }
 }
