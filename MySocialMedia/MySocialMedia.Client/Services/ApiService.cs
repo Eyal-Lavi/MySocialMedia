202f7543c4cd3783c90567efc4050d20d94f31d4
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-08-08 18:41:17 +0300

    Build ApiService and ValidationUiService for the client

---
+++ MySocialMedia/MySocialMedia.Client/Services/ApiService.cs
@@ -0,0 +1,109 @@
+﻿using MySocialMedia.Common.DBTables.MessageDTOs;
+using MySocialMedia.Common.DTOs.UserDTOs;
+using MySocialMedia.Common.DTOs.utlisDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MySocialMedia.Client.Services
+{
+    public class ApiService
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private readonly string _apiURL = "https://localhost:7121/api";
+
+        public ApiService(string apiURL)
+        {
+            _apiURL = apiURL;
+        }
+        public async Task<LoginResponseDTO> LogInAsync(UserLoginDTO loginDataToSend)
+        {
+            string json = JsonSerializer.Serialize(loginDataToSend);
+
+            var content = new StringContent(json, Encoding.UTF8 , "application/json");
+            
+            HttpResponseMessage response = await client.PostAsync($"{_apiURL}/users/login", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true 
+                };
+
+                return JsonSerializer.Deserialize<LoginResponseDTO>(responseBody, options);
+            }
+            else
+            {
+                string errorResponse = await response.Content.ReadAsStringAsync();
+                throw new HttpRequestException($"Error: {response.StatusCode}, Details: {errorResponse}");
+            }
+        }
+        public async Task<bool> RegisterAsync(UserCreationDTO userCreation)
+        {
+            string json = JsonSerializer.Serialize(userCreation);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync($"{_apiURL}/users/register", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                string errorResponse = await response.Content.ReadAsStringAsync();
+                throw new HttpRequestException($"Error: {response.StatusCode}, Details: {errorResponse}");
+            }
+        }
+        public async Task<List<UserMessageDTO>> GetMessagesAsync(string token , string targetUser)
+        {
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            HttpResponseMessage response = await client.GetAsync($"{_apiURL}/messages/allMessages?targetUser={targetUser}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                return JsonSerializer.Deserialize<List<UserMessageDTO>>(responseBody, options);
+            }
+            else
+            {
+                string errorResponse = await response.Content.ReadAsStringAsync();
+                throw new HttpRequestException($"Error: {response.StatusCode}, Details: {errorResponse}");
+            }
+        }
+        public async Task SendMessageAsync(string token, UserMessageCreationDTO messageCreationDTO)
+        {
+            string json = JsonSerializer.Serialize(messageCreationDTO);
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync($"{_apiURL}/messages/send" , content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+            }
+            else
+            {
+                string errorResponse = await response.Content.ReadAsStringAsync();
+                throw new HttpRequestException($"Error: {response.StatusCode}, Details: {errorResponse}");
+            }
+        }
+    }
+}
---
+++ MySocialMedia/MySocialMedia.Client/Services/ValidationUiService.cs
@@ -0,0 +1,132 @@
+﻿using MySocialMedia.Common.DTOs.UserDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySocialMedia.Client.Services
+{
+    public static class ValidationUiService
+    {
+        public static string? Username(string userName)
+        {
+            if(userName == null)
+            {
+                return "Cannot be Null";
+            }
+            if(userName.Length < 2)
+            {
+                return "The Minimum Length is 2";
+            }
+            return null;
+        }
+        public static string? Password(string password)
+        {
+            if (password == null)
+            {
+                return "Cannot be Null";
+            }
+            if (password.Length < 8)
+            {
+                return "The Minimum Length is 8";
+            }
+            if(!password.Any(x => x >= 'A' && x <= 'Z'))
+            {
+                return "Must be 1 charter that is big letter";
+            }
+            if (!password.Any(x => x >= 'a' && x <= 'z'))
+            {
+                return "Must be 1 charter that is small letter";
+            }
+            return null;
+        }
+        public static string? FirstName(string firstName)
+        {
+            if (firstName == null)
+            {
+                return "Cannot be Null";
+            }
+            if (firstName.Length == 0)
+            {
+                return "Cannot be empty";
+            }
+            if (firstName.Any(char.IsDigit))
+            {
+                return "Cannot contain digit";
+            }
+            return null;
+        }
+        public static string? LastName(string lastName)
+        {
+            if (lastName == null)
+            {
+                return "Cannot be Null";
+            }
+            if (lastName.Length == 0)
+            {
+                return "Cannot be empty";
+            }
+            if (lastName.Any(char.IsDigit))
+            {
+                return "Cannot contain digit";
+            }
+            return null;
+        }
+        public static List<string> CheckAllParamsLogin(UserLoginDTO userCreationDTO)
+        {
+            List<string> errors = new List<string>();
+
+            if (userCreationDTO == null)
+            {
+                throw new ArgumentNullException(nameof(userCreationDTO) + " - Cannot be Null");
+            }
+
+            var usernameValiRespo = Username(userCreationDTO.UserName);
+            var passwordValiRespo = Password(userCreationDTO.Password);
+
+            if (usernameValiRespo != null)
+            {
+                errors.Add(usernameValiRespo);
+            }
+            if (passwordValiRespo != null)
+            {
+                errors.Add(passwordValiRespo);
+            }
+            return errors;
+        }
+        public static List<string> CheckAllParamsRegister(UserCreationDTO userCreationDTO)
+        {
+            List<string> errors = new List<string>();
+            if(userCreationDTO == null)
+            {
+                throw new ArgumentNullException(nameof(userCreationDTO) + " - Cannot be Null");
+            }
+
+            var usernameValiRespo = Username(userCreationDTO.UserName);
+            var passwordValiRespo = Password(userCreationDTO.Password);
+            var firstnameValiRespo = FirstName(userCreationDTO.FirstName);
+            var lastnameValiRespo = LastName(userCreationDTO.LastName);
+
+            if(usernameValiRespo != null)
+            {
+                errors.Add(usernameValiRespo);
+            }
+            if(passwordValiRespo != null)
+            {
+                errors.Add(passwordValiRespo);
+            }
+            if (firstnameValiRespo != null)
+            {
+                errors.Add(firstnameValiRespo);
+
+            }
+            if (lastnameValiRespo != null)
+            {
+                errors.Add(lastnameValiRespo);
+            }
+
+            return errors;
+        }
+    }
+}
