20501a3aa2ac6dbc9ebd062c013fe9645d1ec15c
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-08-08 18:43:10 +0300

    Build the Client Side

---
+++ MySocialMedia/MySocialMedia.Client/MainAppWindow.xaml.cs
@@ -0,0 +1,245 @@
+﻿using MySocialMedia.Client.Services;
+using MySocialMedia.Common.DBTables.MessageDTOs;
+using MySocialMedia.Common.DTOs.utlisDTOs;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Net.WebSockets;
+using System.Text;
+using System.Net.Http;
+using System.Text.Json;
+using MySocialMedia.Common.DBTables;
+using System.Windows.Media;
+
+namespace MySocialMedia.Client
+{
+    public partial class MainAppWindow : Window
+    {
+        private readonly LoginResponseDTO _userData;
+        public MainAppViewModel ViewModel { get; set; }
+        
+        private readonly HttpClient _httpClient = new HttpClient { BaseAddress = new Uri("http://localhost:7121/api/") };
+        private ClientWebSocket _clientWebSocket = new ClientWebSocket();
+
+        private readonly ApiService _apiService = new ApiService("https://localhost:7121/api");
+        
+        public MainAppWindow(LoginResponseDTO userData , string username)
+        {
+            InitializeComponent();
+            
+            _userData = userData;
+            ViewModel = new MainAppViewModel();
+            DataContext = ViewModel;
+            lvDataBinding.ItemsSource = _userData.Chats;
+            
+            ViewModel.CurrentUserName = username;
+            userThatLogin_Username.Content = username;
+            
+            ConnectWebSocketWithToken(ViewModel.CurrentUserName);
+
+            //this.Closing += MainAppWindow_Closing;
+        }
+        private async void ConnectWebSocketWithToken(string token)
+        {
+            try
+            {
+                _clientWebSocket.Options.SetRequestHeader("Sec-WebSocket-Protocol", token);
+                await _clientWebSocket.ConnectAsync(new Uri("wss://localhost:7121/ws"), CancellationToken.None);
+
+                _ = Task.Run(() => ReceiveMessagesAsync(_clientWebSocket));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Eyal -> An error occurred: {ex.Message}");
+            }
+        }
+
+        private async Task ReceiveMessagesAsync(ClientWebSocket clientWebSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            while (clientWebSocket.State == WebSocketState.Open)
+            {
+                try
+                {
+                    var result = await clientWebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                    Console.WriteLine($"WebSocket received message with length {result.Count}");
+
+                    if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        await clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                        Console.WriteLine("WebSocket closed by server");
+                    }
+                    else
+                    {
+                        var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                        Console.WriteLine($"Received message: {message}");
+                        var options = new JsonSerializerOptions
+                        {
+                            PropertyNameCaseInsensitive = true
+                        };
+                        
+                        var messageObject = JsonSerializer.Deserialize<UserMessageDTO>(message, options);
+   
+                        Application.Current.Dispatcher.Invoke(() =>
+                        {
+                            if(ViewModel.SelectedChat?.ChatId == messageObject?.SenderUserId)
+                            {
+                                messageObject.SendUser = new User { USER_NAME = ViewModel?.SelectedChat?.ChatName };
+                                ViewModel.UserMessages.Add(messageObject);
+                                ScrollMessagesToEnd();
+                            }
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error in ReceiveMessagesAsync: {ex.Message}");
+                }
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private async void ListViewContancs_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Border border && border.DataContext is ChatSummaryDTO data)
+            {
+                if (ViewModel.SelectedChat != null && ViewModel.SelectedChat.ChatName == data.ChatName)
+                {
+                    return;
+                }
+                ViewModel.SelectedChat = data; // עדכון הצ'אט הנבחר
+                var response = await _apiService.GetMessagesAsync(_userData.AuthToken, data.ChatName);
+                ViewModel.UserMessages = new ObservableCollection<UserMessageDTO>(response);
+                ChatBlock.Visibility = Visibility.Visible;
+                UsernameTitleChat.Content = $"@{data.ChatName}";
+                EnterMessageBox.Text = String.Empty;
+                ScrollMessagesToEnd();
+            }
+        }
+        private async void ImageSend_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            string userMessage = EnterMessageBox.Text;
+            
+            try
+            {
+                await _apiService.SendMessageAsync(_userData.AuthToken, new UserMessageCreationDTO
+                {
+                    SenderUserId = _userData.UserId,
+                    ReceiverUserId = ViewModel.SelectedChat.ChatId,
+                    MessageData = userMessage,
+                });
+                ViewModel.UserMessages.Add(new UserMessageDTO
+                {
+                    SendUser = new User { USER_NAME = ViewModel.CurrentUserName},
+                    MessageData = userMessage,
+                    MessageDate = DateTime.Now,
+                    SenderUserId = _userData.UserId,
+                    ReceiverUserId = ViewModel.SelectedChat.ChatId,
+                });
+                ScrollMessagesToEnd();
+                EnterMessageBox.Text = String.Empty;
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot Send the message -" + ex.Message);
+            }
+
+            e.Handled = true;
+        }
+        private void LogoutButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MainAppWindow_Closing(sender, e);
+            OpenMainWindow();
+        }
+        private void OpenMainWindow()
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+        private void ScrollMessagesToEnd()
+        {
+            if(messagesDataList.Items.Count > 0)
+            {
+                var border = VisualTreeHelper.GetChild(messagesDataList, 0) as Decorator;
+
+                if (border != null)
+                {
+                    var scrollViewer = border.Child as ScrollViewer;
+
+                    scrollViewer?.ScrollToEnd();
+                }
+            }
+        }
+        private async void MainAppWindow_Closing(object sender, MouseButtonEventArgs e)
+        {
+            if (_clientWebSocket != null && _clientWebSocket.State == WebSocketState.Open)
+            {
+                await _clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+                _clientWebSocket.Dispose();
+            }
+        }
+
+        private void ShutDownImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MainAppWindow_Closing(sender, e);
+            this.Close();
+        }
+
+        private void NewContactMessage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+    }
+}
+
+public class MainAppViewModel : INotifyPropertyChanged
+{
+    private ObservableCollection<UserMessageDTO> _userMessages;
+    private ChatSummaryDTO _selectedChat;
+    private string _currentUserName;
+
+    public ObservableCollection<UserMessageDTO> UserMessages
+    {
+        get { return _userMessages; }
+        set
+        {
+            _userMessages = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ChatSummaryDTO SelectedChat
+    {
+        get { return _selectedChat; }
+        set
+        {
+            _selectedChat = value;
+            OnPropertyChanged();
+        }
+    }
+    public string CurrentUserName
+    {
+        get { return _currentUserName; }
+        set
+        {
+            _currentUserName = value;
+            OnPropertyChanged();
+        }
+    }
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
--- MySocialMedia/MySocialMedia.Client/MainWindow.xaml.cs
+++ MySocialMedia/MySocialMedia.Client/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using MySocialMedia.Client.Services;
+using MySocialMedia.Common.DTOs.UserDTOs;
+using MySocialMedia.Common.DTOs.utlisDTOs;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +17,140 @@ using System.Windows.Shapes;
 
 namespace MySocialMedia.Client
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        private readonly ApiService _apiService = new ApiService("https://localhost:7121/api");
+        private bool _loginPremisson = true;
+        private bool _registerPremisson = true;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void MainWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void CreateAccount_Click(object sender, RoutedEventArgs e)
+        {
+            Register.Visibility = Visibility.Visible;
+            Login.Visibility = Visibility.Hidden;
+        }
+
+        private void Already_have_an_account_Click(object sender, RoutedEventArgs e)
+        {
+            Login.Visibility = Visibility.Visible;
+            Register.Visibility = Visibility.Hidden;
+        }
+
+        private async void LogIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_loginPremisson == false)
+            {
+                return;
+            }
+            _loginPremisson = false;
+
+            var loginDataReq = new UserLoginDTO
+            {
+                UserName = EnterUserName.Text,
+                Password = EnterPassword.Text,
+            };
+
+            try
+            {
+                var listErrors = ValidationUiService.CheckAllParamsLogin(loginDataReq);
+
+                if (listErrors.Count != 0)
+                {
+                    ErrorMessageLogin.Visibility = Visibility.Visible;
+                    _loginPremisson = true;
+                    return;
+                }
+
+                var loginResponse = await _apiService.LogInAsync(loginDataReq);
+
+                if (loginResponse != null)
+                {
+                    _loginPremisson = true;
+                    OpenMainAppWindow(loginResponse, loginDataReq.UserName);
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessageLogin.Visibility = Visibility.Visible;
+                _loginPremisson = true;
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void OpenMainAppWindow(LoginResponseDTO userData,string username)
+        {
+            var mainAppWindow = new MainAppWindow(userData, username);
+            mainAppWindow.Show();
+            this.Close(); // Close the current window
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private async void Confirm_reg_Click(object sender, RoutedEventArgs e)
+        {
+            if (_registerPremisson == false)
+            {
+                return;
+            }
+            _registerPremisson = false;
+            var userCreation = new UserCreationDTO
+            {
+                UserName = EnterUsername_Reg.Text,
+                FirstName = EnterFirstName_Reg.Text,
+                LastName = EnterLastName_Reg.Text,
+                Password = EnterPassword_Reg.Text,
+            };
+
+            try
+            {
+                var listErrors = ValidationUiService.CheckAllParamsRegister(userCreation);
+                if (listErrors.Count != 0)
+                {
+                    _registerPremisson = true;
+                    return;
+                }
+
+                var response = await _apiService.RegisterAsync(userCreation);
+
+                if (response)
+                {
+                    _registerPremisson = true;
+                    MessageBox.Show("Register Complete");
+                }
+            }
+            catch (Exception ex)
+            {
+                _registerPremisson = true;
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void EnterUserName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(ErrorMessageLogin != null)
+            {
+                ErrorMessageLogin.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void EnterPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (ErrorMessageLogin != null)
+            {
+                ErrorMessageLogin.Visibility = Visibility.Hidden;
+            }
         }
     }
 }
